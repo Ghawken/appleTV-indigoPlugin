5156716b456cea4da51de8f593cac71e125e96be
Glenn Hawken <me@gizmocoding.com>
2025-03-09 16:26:15 +1100

    1.5.7
    Update pyatv library to specific commit - pending another release.  Avoids breaking anything in future.
    
    Updates pyatv library to 0.16.0
    Update dependencies.  Removal unneeded dependencies.
    
    Notably improvement live power state updates (correct).  This historically has been an issue with appleTV devices.   Seems to be correctly updating immediately on my testing.
    
    Add a few Device States
    CurrentlyPlaying : EpisodeNumber, Season, SeriesName, and content Identifier (which is a combo of factors depending on the app playing)
    &
    Add ArtworkID - which changes if new artwork - so can trigger on this if downloading artwork
    
    Refactor some of the command handling now AG should run all commands, including parameters okay
    
    AG - Help for Command help logging
    
    Add TouchGesture Action Group support.
    (for these and others send the parameters in the optional box comma seperated.  eg.
    0,100,500
    
    Add KeyBoard Support
    
    Add/Extend logging menu item

--- appleTV.indigoPlugin/Contents/Server Plugin/plugin.py
+++ appleTV.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -1762,7 +1762,7 @@ class Plugin(indigo.PluginBase):
                 artworkID = atv.metadata.artwork_id
             except Exception as e:
                 self.logger.debug(f"Unable to retrieve artwork id: {e}")
-                artworkID = None  # oFixg
+                artworkID = None
 
             if playstatus.device_state != None:
                 state = playstatus.device_state
